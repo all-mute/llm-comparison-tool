b79d8e2dc801c0fb00dc93c2aea6571fb4828f0f
All Mute ✓ <davidmerkulov@yandex.ru>
2024-04-11 20:43:28 +0300

    init

--- pages/metrics.py
+++ pages/metrics.py
@@ -1,3 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Создаем тестовые данные для таблиц
+data = {
+    'Model': ['Eng_to_Lij_w2vpre', 'Eng_to_Mag_w2vpre', 'Eng_to_Ace_w2vpre', 'Eng_to_Dik_w2vpre',
+              'Eng_to_Lij_facebook', 'Eng_to_Mag_facebook', 'Eng_to_Ace_facebook', 'Eng_to_Dik_facebook'],
+    'BLUE': [2.90, 4.34, 0.5, 0.76, 9.53, 25.89, 0.71, 2.39],
+    'chrf': [28.58, 28.67, 20.16, 19.86, 39.04, 56.00, 0.34, 22.26],
+    'COMET': [0.463, 0.472, 0.547, 0.582, 0.579, 0.655, 0.29, 0.534]
+}
+
+# Преобразуем словарь в DataFrame
+df = pd.DataFrame(data)
+
+# Создаем вкладки для каждого языка с более красивыми названиями
+tab_labels = ["Lij", "Mag", "Ace", "Dik"]
+tab_names = ["Язык Lij", "Язык Mag", "Язык Ace", "Язык Dik"]
+tabs = st.tabs(tab_names)
+
 for i, tab in enumerate(tabs):
     with tab:
         # Фильтруем DataFrame по языку
@@ -29,3 +50,4 @@ for i, tab in enumerate(tabs):
             st.dataframe(result_df)
         else:
             st.write("Для сравнения должно быть ровно две модели.")
+
